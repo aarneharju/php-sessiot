0d1124d2babb7fdf0670a0c3b023f8b043db5d3c
aarneharju <aarne@aarneharju.com>
2022-09-14 13:56:02 +0300

    Added basic styling

---
+++ 1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="1.php">
+        <fieldset>
+            <legend>Set session variables:</legend>
+            <label for="key">Key:</label>
+            <input type="text" name="key" id="key">
+            <label for="key">Value:</label>
+            <input type="text" name="value" id="value">
+            <input type="submit" value="Set session variable">
+        </fieldset>
+    </form>
+
+    <?php
+    
+
+    
+    ?>
+
+</body>
+</html>
