aac5a3d613f84928829c80c74c8360b8370569e2
aarneharju <aarne@aarneharju.com>
2022-09-14 14:56:02 +0300

    Implemented add session variable form

--- 1.php
+++ 1.php
@@ -1,5 +1,8 @@
+<?php session_start() ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +10,45 @@
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="1.php">
+    <form action="1.php" method="post">
         <fieldset>
             <legend>Set session variables:</legend>
             <label for="key">Key:</label>
             <input type="text" name="key" id="key">
-            <label for="key">Value:</label>
+            <label for="value">Value:</label>
             <input type="text" name="value" id="value">
-            <input type="submit" value="Set session variable">
+            <input type="submit" name="addSessionVariable" value="Add session variable">
         </fieldset>
     </form>
 
     <?php
-    
+    // echo "<pre>";
+    // echo var_export($_POST);
+    // echo "</pre>";
 
-    
+
+
+    if (!empty($_POST['addSessionVariable'])) {
+        if (!empty($_POST['key']) && !empty($_POST['value'])) {
+            $_SESSION[$_POST['key']] =  $_POST['value'];
+        }
+    }
+
+    if (!empty($_SESSION)) {
+        echo "<ul>";
+        foreach ($_SESSION as $sessionVariableKey => $sessionVariableValue) {
+            echo "<li>{$sessionVariableKey}: {$sessionVariableValue}</li>";
+        }
+        echo "</ul>";
+
+        // echo '<a href="1.php?clearSessionVariables=true">Clear session variables</a>';
+    }
+
+
     ?>
 
 </body>
+
 </html>
